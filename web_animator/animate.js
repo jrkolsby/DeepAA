2bd6c6713f890c269e4c3c81ac3a3d7f19947bd4
James Kolsby <jrkolsby@mac.com>
2022-04-05 15:45:07 -0400

    add web animator

--- web_animator/animate.js
+++ web_animator/animate.js
@@ -1,7 +1,13 @@
 var content = document.querySelector('#container');
+content.innerText = "Hey! Upload some txt files from frames_*"
 var frames = [];
 var frameIndex = 0;
 var reader = new FileReader();
+
+function utf8_to_b64(str) {
+    const jamesStr = str.replace(/[\u000A\u000D\u000B\u000C\u000D\u001C\u001D\u001E\u0085\u2028\u2029]/g, 'james');
+    return window.btoa(unescape(encodeURIComponent(jamesStr)))
+}
 
 function loadFrames(i) {
     var files = document.querySelector('input[type=file]').files;
@@ -11,6 +17,7 @@ function loadFrames(i) {
     if (i < files.length) {
         reader.onload = function(e) {
             frames.push(e.target.result);
+            console.log(utf8_to_b64(e.target.result));
             loadFrames(i+1);
         }
         reader.readAsText(files[i])
